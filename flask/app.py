ed422b052073825f531ff54a63fdcac7202c4a65
Ali <mojtabavi99@yahoo.com>
2020-08-31 00:30:52 +0430

    flask-write the code

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask,jsonify,request
 app = Flask(__name__)
 
 
-@app.route('/')
-def main_page():
-    return 'Hello'
+
+
+@app.route('/v1/process')
+def process():
+    text = request.args.get('text')
+    print(text)
+    data = {"message":"ok",
+            "text":text}
+    return jsonify(data), 200
+
+def sendsms():
+    pass
+
+def check_serial():
+    pass
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",5000,debug=True)
