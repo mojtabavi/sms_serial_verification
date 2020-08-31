2703376452aa91c4a1cce78a8f2980f027e4fba5
Ali <mojtabavi99@yahoo.com>
2020-08-31 11:21:54 +0430

    node.js-receive message from sms.ir and process it

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('Hello')
+app.use(express.json())
+
+app.get('/v1/process',(req,res) => {
+    const message = req.query.text;
+    const sender = req.query.from;
+    console.log(`receive ${message} from ${sender}`);
+    res.status(200).send({message:'processed'});
 })
 
-app.listen(3000,() => console.log('Listening to port 3000 ... '))
+app.listen(5000,() => console.log('Listening to port 5000 ... '))
