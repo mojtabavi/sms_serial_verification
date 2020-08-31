12d1f5da82569b88607ce71f2554f8645627f50a
Ali <mojtabavi99@yahoo.com>
2020-09-01 01:58:57 +0430

    node.js-convert excel to database

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sendsms = require("./sms")
-const readXlsxFile = require('read-excel-file/node');
+const xlsxFile = require("read-excel-file/node");
+const sqlite3 = require("sqlite3").verbose();
 const app = express();
 
 
@@ -13,15 +14,38 @@ app.get('/v1/process',(req,res) => {
     res.status(200).send({message:'processed'});
 })
 
-async function excelParser(){
-    
+let db = new sqlite3.Database("../data.sqlite", sqlite3.OPEN_READWRITE, (err) => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    console.log("Connected to the SQLite database.");
+  }
+});
 
-    // File path.
-    const rows = await readXlsxFile('../data.xlsx');
-    print(rows)
-}
+db.run("DROP TABLE IF EXISTS serials");
+db.run("CREATE TABLE IF NOT EXISTS serials (id INTEGER PRIMARY KEY,ref TEXT,desc TEXT,start_serial TEXT,end_serial TEXT,date DATE);");
+
+xlsxFile("../data.xlsx").then((rows) => {
+    const header = rows.splice(0,1);
+    rows.forEach(row => {
+        // let placeholders = `(${row.map((value) => value).join(',')})`
+        // let sql = "INSERT INTO serials VALUES " + placeholders;
+        db.run(`INSERT INTO serials VALUES(?,?,?,?,?,?)`, row, function (err) {
+          if (err) {
+            return console.log(err.message);
+          }
+          // get the last insert id
+          console.log(`A row has been inserted with rowid ${this.lastID}`);
+        });
+        // console.log(sql)
+        // db.run(sql)
+    });
+    db.close();
+});  
+
+
+
 
 // sendsms("09392115688", "تست ارسال به تلفن همراه");
-excelParser()
 app.listen(5000,() => console.log('Listening to port 5000 ... '));
 
