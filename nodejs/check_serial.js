3d5d7b543afde155149b7244cda59a454e7c09be
Ali <mojtabavi99@yahoo.com>
2020-09-05 14:59:28 +0430

    node.js-check_serial

--- nodejs/check_serial.js
+++ nodejs/check_serial.js
@@ -2,23 +2,15 @@ const config = require("./config");
 const sqlite3 = require("sqlite3").verbose();
 
 function checkSerial(serial){
+    // 0 => not find
+    // 1 => find and valid
+    // 2 find and invalid
+    let valid = null
     let db = new sqlite3.Database("../data.sqlite", sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
             console.error(err.message);
         } else {
             console.log("Connected to the SQLite database.");
-        }
-    });
-
-    let query = `SELECT * FROM serials WHERE start_serial < "${serial}" AND end_serial > "${serial}";`
-// first row only
-    db.all(query, [], (err, rows) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        if (rows.length === 1) {
-            db.close();
-           return "I found your serial";
         }
     });
 
@@ -28,14 +20,34 @@ function checkSerial(serial){
             return console.error(err.message);
         }
         if (rows.length === 1) {
-            db.close();
-            return "the serial is among failed ones";
+            valid = false
+            console.log("the serial is among failed ones");
+        }
+        else if(rows.length === 0){
+            valid = true
         }
     });
-    db.close();
-    return "it was not in the db";
+
+
+    let query = `SELECT * FROM serials WHERE start_serial < "${serial}" AND end_serial > "${serial}";`
+// first row only
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        if (rows.length === 1 && valid !== false) {
+            valid = true
+           console.log("I found your serial");
+        }
+        else if(rows.length === 0){
+            console.log("Your serial was not in db")
+        }
+    });
+    
+
+db.close()
 
 
 }
 
-checkSerial("JJ1000003")
+checkSerial("JJ101");
