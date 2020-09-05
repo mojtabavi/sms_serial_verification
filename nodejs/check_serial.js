671208e6a825a754a140777f348144dcab7aebbe
Ali <mojtabavi99@yahoo.com>
2020-09-05 16:14:56 +0430

    node.js-better and simple codes for checkserial with sqlite-sync

--- nodejs/check_serial.js
+++ nodejs/check_serial.js
@@ -1,53 +1,20 @@
 const config = require("./config");
 const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite-sync");
 
 function checkSerial(serial){
-    // 0 => not find
-    // 1 => find and valid
-    // 2 find and invalid
-    let valid = null
-    let db = new sqlite3.Database("../data.sqlite", sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            console.error(err.message);
-        } else {
-            console.log("Connected to the SQLite database.");
-        }
-    });
 
-    let sql = `SELECT * FROM invalids WHERE invalid_serial = "${serial}"`
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        if (rows.length === 1) {
-            valid = false
-            console.log("the serial is among failed ones");
-        }
-        else if(rows.length === 0){
-            valid = true
-        }
-    });
+    sqlite.connect('../data.sqlite');
 
-
-    let query = `SELECT * FROM serials WHERE start_serial < "${serial}" AND end_serial > "${serial}";`
-// first row only
-    db.all(query, [], (err, rows) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        if (rows.length === 1 && valid !== false) {
-            valid = true
-           console.log("I found your serial");
-        }
-        else if(rows.length === 0){
-            console.log("Your serial was not in db")
-        }
-    });
-    
-
-db.close()
+    let query = sqlite.run(`SELECT * FROM invalids WHERE invalid_serial = "${serial}"`);
+    if (query.length === 1) {
+        return "the serial is among failed ones";
+    }
+    query = sqlite.run(`SELECT * FROM serials WHERE start_serial < "${serial}" AND end_serial > "${serial}";`);
+    if (query.length === 1) {
+        return  "I found your serial";
+    }
+    return "Your Serial is not in db"
 
 
 }
-
-checkSerial("JJ101");
