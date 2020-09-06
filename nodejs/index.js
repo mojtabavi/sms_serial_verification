cbb3cbb2a5d1fa46db6d1a4b03fcbf63eb3dd4a4
Ali <mojtabavi99@yahoo.com>
2020-09-06 13:38:27 +0430

    flask-add sb-admin-2 files to project

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,6 +3,18 @@ const main = require('./routes/apiv1');
 const upload = require('./routes/uploadFile')
 const app = express();
 const path = require("path")
+const rateLimit = require("express-rate-limit");
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const apiLimiter = rateLimit({
+    max: 5
+});
+
+// only apply to requests that begin with /api/
+
 
 
 app.use(express.json());
@@ -11,6 +23,7 @@ app.set("view engine","ejs")
 app.set("view engine","ejs")
 app.use("/v1/process",main);
 app.use("/file",upload);
+app.use("/file",apiLimiter);
 
 
 
